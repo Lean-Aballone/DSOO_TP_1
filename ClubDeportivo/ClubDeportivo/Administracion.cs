8cd8dee55e8bc65bd3834c6098060ba57a0b3df9
Lean-Aballone <l.aballone@gmail.com>
2024-09-24 18:08:59 -0300

    Se implementa el metodo ActualizarBBDD de la interfaz IAdministracion.

--- ClubDeportivo/ClubDeportivo/Administracion.cs
+++ ClubDeportivo/ClubDeportivo/Administracion.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClubDeportivo
 {
-    class Administracion
+    class Administracion : IAdministracion
     {
         private const string nombre = "Club Deportivo";
         private List<Socios> Activos;           //  ****
@@ -14,5 +14,14 @@ namespace ClubDeportivo
         private List<NoSocios> Aspirantes;      //  ****
         public string Nombre { get { return nombre; } }
 
+        public void ActualizarBBDD(List<Socios> activ, List<Socios> inact) {
+            //TODO: implementar pase de NoSocio a socio (remover '?')
+            
+            inact.ForEach(socio => { if (socio.Activo.Equals(true)) activ.Add(socio); });
+            inact.RemoveAll(socio => socio.Activo.Equals(true));
+
+            activ.ForEach(socio => { if (socio.Activo.Equals(false)) inact.Add(socio); });
+            activ.RemoveAll(socio => socio.Activo.Equals(false));
+        }
     }
 }
