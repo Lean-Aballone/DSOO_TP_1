cc17c8c401a3d08e2dc202ecedb6bf98566ad150
Lean-Aballone <l.aballone@gmail.com>
2024-09-24 18:07:42 -0300

    Se crea la clase Administracion con propiedades para el registro de socios y no socios.

---
+++ ClubDeportivo/ClubDeportivo/Administracion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDeportivo
+{
+    class Administracion
+    {
+        private const string nombre = "Club Deportivo";
+        private List<Socios> Activos;           //  ****
+        private List<Socios> Inactivos;         //  REGISTRO DE LOS SOCIOS Y NO SOCIOS.
+        private List<NoSocios> Aspirantes;      //  ****
+        public string Nombre { get { return nombre; } }
+
+    }
+}
