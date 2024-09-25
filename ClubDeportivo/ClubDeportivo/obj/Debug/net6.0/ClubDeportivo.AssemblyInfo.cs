8a37813dd8ef55ccc8ee0168a9df324c541ef772
Lean-Aballone <l.aballone@gmail.com>
2024-09-25 20:36:07 -0300

    Se renombra clase Administracion ahora ClubDeportivo y se incorpora metodo altaSocio

--- ClubDeportivo/ClubDeportivo/obj/Debug/net6.0/ClubDeportivo.AssemblyInfo.cs
+++ ClubDeportivo/ClubDeportivo/obj/Debug/net6.0/ClubDeportivo.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ClubDeportivo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0349a95b9d6722750629f755a293350be53eeb8e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b3abe8a8d8da920e0254662c7c3954bacbddff9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ClubDeportivo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ClubDeportivo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
