3d3820aa631064b0cfc436dd43385e5683762a26
Lean-Aballone <l.aballone@gmail.com>
2024-09-25 21:50:09 -0300

    Se crea método InscribirActividad (Desarrollo pendiente).

--- ClubDeportivo/ClubDeportivo/IAdministracion.cs
+++ ClubDeportivo/ClubDeportivo/IAdministracion.cs
@@ -7,5 +7,7 @@ using System.Threading.Tasks;
 namespace ClubDeportivo {
     internal interface IAdministracion {
         void ActualizarBBDD(List<Socios> a, List<Socios> i);//TODO: , List<NoSocios> ns); implementar pase de NoSocio a socio.
+        void AltaSocio(NoSocios noSocio);
+        string InscribirActividad(string actividadDeportiva, int idSocio);
     }
 }
