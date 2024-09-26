3561eef88361a74df62e0e7769bc01fa3be582ab
Lean-Aballone <l.aballone@gmail.com>
2024-09-25 21:50:09 -0300

    Se crea método InscribirActividad (Desarrollo pendiente).

--- ClubDeportivo/ClubDeportivo/CLubDeportivo.cs
+++ ClubDeportivo/ClubDeportivo/CLubDeportivo.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,8 +11,6 @@ namespace ClubDeportivo
     {
 
         private const int TOPEACTIVIDADESPORSOCIO = 3;
-
-
         private List<Socios> Activos;           //  ****
         private List<Socios> Inactivos;         //  REGISTRO DE LOS SOCIOS Y NO SOCIOS.
         private List<NoSocios> Aspirantes;      //  ****
