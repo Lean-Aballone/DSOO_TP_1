aa5217b372638b4dab4cb8a4d5d64ddde967686d
Lean-Aballone <l.aballone@gmail.com>
2024-09-28 20:50:05 -0300

    Se comenta codigo no desarrollado, Se agrega Apellido y DNI como atributos de socio, se implementa constructor

--- ClubDeportivo/ClubDeportivo/Socios.cs
+++ ClubDeportivo/ClubDeportivo/Socios.cs
@@ -6,34 +6,45 @@ using System.Threading.Tasks;
 
 namespace ClubDeportivo {
     internal class Socios {
-        public int Id { get; set; }
+        public uint Id { get; set; }
         public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public uint DNI { get; set; }
         public bool Activo { get; set; }
         public DateTime FechaIngreso { get; set; } // Fecha en la que el socio se unió al club
-        public List<Clases> ClasesInscritas { get; set; } // Lista de clases a las que está inscrito el socio
-        public List<Pagos> HistorialPagos { get; set; } // Lista de pagos realizados por el socio
-        public FichaMedica FichaMedica { get; set; } // Información médica del socio
-        public int MaxClases { get; set; } = 3; // Máximo de clases que puede tomar el socio
+       // public List<Clases> ClasesInscritas { get; set; } // Lista de clases a las que está inscrito el socio
+      //  public List<Pagos> HistorialPagos { get; set; } // Lista de pagos realizados por el socio
+     //   public FichaMedica FichaMedica { get; set; } // Información médica del socio
 
         public Socios() {
-            ClasesInscritas = new List<Clases>();
-            HistorialPagos = new List<Pagos>();
+//            ClasesInscritas = new List<Clases>();
+ //           HistorialPagos = new List<Pagos>();
+        }
+
+        public Socios(string Nombre, string Apellido, uint dni, uint Id) {
+            this.Id = Id + 1;
+            this.Nombre = Nombre;
+            this.Apellido = Apellido;
+            DNI = dni;
+            FechaIngreso = DateTime.Now;
+            Console.WriteLine("\nEl Socio: " + this.Apellido +", "+ this.Nombre + " DNI: " + dni + " Fue Ingresado Exitosamente.");
+            Console.WriteLine("Con Nro de ID: " + this.Id);
         }
 
         // Método para verificar si el socio puede inscribirse en más clases
         public bool PuedeInscribirseEnClase() {
-            return ClasesInscritas.Count < MaxClases;
+            return true;// ClasesInscritas.Count < MaxClases;
         }
-
+/*
         // Método para agregar una clase
         public void InscribirseEnClase(Clases nuevaClase) {
             if (PuedeInscribirseEnClase()) {
-                ClasesInscritas.Add(nuevaClase);
+               ClasesInscritas.Add(nuevaClase);
             } else {
                 throw new InvalidOperationException("No se puede inscribir en más de 3 clases.");
             }
         }
-
+      
         // Método para verificar si el socio está al día con los pagos
         public bool EstaAlDia() {
             return HistorialPagos.All(p => p.Estado == true);
@@ -43,5 +54,5 @@ namespace ClubDeportivo {
         public int ObtenerCantidadDeClases() {
             return ClasesInscritas.Count;
         }
-    }
+    */}
 }
