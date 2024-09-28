f2ce69a1cba9cb019a6803addb4fb8dbf877bc6b
Lean-Aballone <l.aballone@gmail.com>
2024-09-28 20:48:23 -0300

    Se desarrolla clase Menu, se desarrolla menu de socios, Se agrega funcionalidad para ingresar socios y registrarlos.

--- ClubDeportivo/ClubDeportivo/CLubDeportivo.cs
+++ ClubDeportivo/ClubDeportivo/CLubDeportivo.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace ClubDeportivo
 {
-    class CLubDeportivo : IAdministracion
+    class ClubDeportivo : IAdministracion
     {
 
         private const int TOPEACTIVIDADESPORSOCIO = 3;
@@ -26,16 +26,31 @@ namespace ClubDeportivo
             */
 
            
-            if(!Activos.Exists(socio => socio.ID.Equals(idSocio))) return "SOCIO INEXISTENTE"; // el socio no se encuentra registrado dentro de la colección de socios en el club deportivo.
+            if(!Activos.Exists(socio => socio.Id.Equals(idSocio))) return "SOCIO INEXISTENTE"; // el socio no se encuentra registrado dentro de la colección de socios en el club deportivo.
+            if (!Inactivos.Exists(socio => socio.Id.Equals(idSocio))) return "EL SOCIO NO SE ENCUENTRA ACTIVO";
             //TODO: if(Plantel.Actividades.Exists( actividad => actividad.nombre:Equals(actividadDeportiva))) return "ACTIVIDAD INEXISTENTE"; // la actividad deportiva no se encuentra dentro de la colección de actividades en el club deportivo.
-            if(Activos.Find(socio => socio.ID.Equals(idSocio)).ClasesInscritas.Count >= TOPEACTIVIDADESPORSOCIO) return "TOPE DE ACTIVIDADES ALCANZADO"; //cuando el socio ya participa en tres actividades deportivas
+           // if(Activos.Find(socio => socio.Id.Equals(idSocio)).ClasesInscritas.Count >= TOPEACTIVIDADESPORSOCIO) return "TOPE DE ACTIVIDADES ALCANZADO"; //cuando el socio ya participa en tres actividades deportivas
 
             //TODO: Agregar la actividadDeportiva de Plantel.Actividades a la lista de actividades del socio e Imprimir mensaje Inscripcion exitosa.
 
             return "INSCRIPCIÓN EXITOSA";
         }
         public void AltaSocio(NoSocios noSocios) {
-            Activos.Add(new Socios(true, noSocios.Nombre));
+         //   Activos.Add(new Socios(true, noSocios.Nombre));
+        }
+
+        public void AltaSocio(string nombre, string apellido, uint dni) {
+
+            if (Activos.Exists(socio => socio.DNI.Equals(dni))) { 
+                Console.WriteLine("\nEl Socio ya se encuentra registrado.");
+                return;
+            }
+            if (Inactivos.Exists(socio => socio.DNI.Equals(dni))) {
+                Console.WriteLine("\nEl Socio ya se encuentra registrado.");
+                return;
+            }
+            if (Aspirantes.Exists(aspirante => aspirante.DNI.Equals(dni))) Aspirantes.RemoveAll(aspirante => aspirante.DNI.Equals(dni));
+            Inactivos.Add(new Socios(nombre, apellido, dni, (uint)Inactivos.Count));
         }
         public void ActualizarBBDD(List<Socios> activ, List<Socios> inact) {
             //TODO: implementar pase de NoSocio a socio (remover '?')
@@ -45,6 +60,18 @@ namespace ClubDeportivo
 
             activ.ForEach(socio => { if (socio.Activo.Equals(false)) inact.Add(socio); });
             activ.RemoveAll(socio => socio.Activo.Equals(false));
+        }
+
+        public ClubDeportivo(List<Socios> activos, List<Socios> inactivos, List<NoSocios> aspirantes) { //Futura BBDD
+            Activos = activos;
+            Inactivos = inactivos;
+            Aspirantes = aspirantes;
+        }
+
+        public ClubDeportivo() {
+            this.Activos = new List<Socios>();       //  ****
+            this.Inactivos = new List<Socios>();     //  REGISTRO DE LOS SOCIOS Y NO SOCIOS.
+            this.Aspirantes = new List<NoSocios>();  //  
         }
     }
 }
--- ClubDeportivo/ClubDeportivo/NoSocios.cs
+++ ClubDeportivo/ClubDeportivo/NoSocios.cs
@@ -7,6 +7,8 @@ using System.Threading.Tasks;
 namespace ClubDeportivo {
     internal class NoSocios {
         public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public uint DNI { get; set; }
 
         public NoSocios(string nombre) {
             Nombre = nombre;
