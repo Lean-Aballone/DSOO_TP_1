d41933ab182d61324f0af3c17e718eac91ac7952
Lean-Aballone <l.aballone@gmail.com>
2024-09-27 22:39:45 -0300

    main loop y clase Menu (Desarrollo pendiente)

---
+++ ClubDeportivo/ClubDeportivo/Menu.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubDeportivo {
+    internal class Menu {
+        public static sbyte Run() {
+            Console.WriteLine("\nMenú de opciones.\n");
+            Console.WriteLine("\t 1. Gestionar socios.");
+            Console.WriteLine("\t 2. Gestionar postulantes.");
+            Console.WriteLine("\t 3. Gestionar Plantel.");
+            Console.WriteLine("\t 0. Pulse 0 (cero) para salir.");
+            Console.Write("\nIngrese el número de menú: ");
+            return EntradaValidacion(Convert.ToSByte(Console.ReadLine()));
+        }
+
+        public static void Socios() {
+            Console.Clear();
+            Console.WriteLine("Menú socios");
+        }
+
+        public static void NoSocios() {
+            Console.Clear();
+            Console.WriteLine("Menú postulantes");
+        }
+
+        public static void Plantel() {
+            Console.Clear();
+            Console.WriteLine("Menú Plantel");
+        }
+
+        private static sbyte EntradaValidacion(sbyte opc) {
+            if (opc < 0 || opc > 3) {
+                Console.WriteLine("\n\t\tError el digito ingresado no corresponde con una opcion valida.");
+                return -1;
+            }
+            return opc;
+        }
+    }
+}
--- ClubDeportivo/ClubDeportivo/Program.cs
+++ ClubDeportivo/ClubDeportivo/Program.cs
@@ -1,34 +1,26 @@
 namespace ClubDeportivo {
     internal class Program {
+        CLubDeportivo admin = new CLubDeportivo();
         static void Main(string[] args) {
-            // Crear una instancia de un profesor
-            Profesores profesor = new Profesores {
-                Id = 1,
-                Nombre = "Carlos",
-                Actividad = "Entrenador"
-            };
+            sbyte opc;
+            do {
+                opc = Menu.Run();
+                switch (opc) {
+                    case 1:
+                        Menu.Socios();
+                        break;
+                    case 2:
+                        Menu.Socios();
+                        break;
+                    case 3:
+                        Menu.Plantel();
+                        break;
+                    default:
+                        Console.Clear();
+                        break;    
+                }            
+            } while(opc != 0);
 
-            // Crear una instancia de un socio
-            Socio socio = new Socio {
-                Id = 100,
-                Nombre = "Juan"
-            };
-
-            // Crear un turno para el socio
-            Turno turno = new Turno {
-                Fecha = DateTime.Now,
-                Hora = new TimeSpan(9, 0, 0),
-                Socio = socio
-            };
-
-            // Usar métodos de la clase profesor
-            profesor.RegistrarAsistencia();
-            double salario = profesor.CalcularSalarioMensual();
-
-            // Mostrar resultados en consola
-            Console.WriteLine($"El profesor {profesor.Nombre} ha registrado asistencia.");
-            Console.WriteLine($"El salario calculado del profesor es: {salario}");
-            Console.WriteLine($"El socio {socio.Nombre} tiene un turno asignado el {turno.Fecha} a las {turno.Hora}");
         }
     }
 }
