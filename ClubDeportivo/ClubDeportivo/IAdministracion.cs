8a546644b85f50d42e54a1d4c4c5c778d4f20bd9
Lean-Aballone <l.aballone@gmail.com>
2024-09-29 19:03:36 -0300

    Se agrega overload para AltaSocio

--- ClubDeportivo/ClubDeportivo/IAdministracion.cs
+++ ClubDeportivo/ClubDeportivo/IAdministracion.cs
@@ -8,6 +8,7 @@ namespace ClubDeportivo {
     internal interface IAdministracion {
         void ActualizarBBDD(List<Socios> a, List<Socios> i);//TODO: , List<NoSocios> ns); implementar pase de NoSocio a socio.
         void AltaSocio(NoSocios noSocio);
+        void AltaSocio(string nombre, string apellido, uint dni);
         string InscribirActividad(sbyte actividadDeportiva, int idSocio);
         string InscribirActividad(sbyte actividadDeportiva, uint dni);
 
