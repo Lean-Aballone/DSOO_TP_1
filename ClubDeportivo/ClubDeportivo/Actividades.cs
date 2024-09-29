6866ff6852389f4fd8be1c767ebf44a107a5f491
Lean-Aballone <l.aballone@gmail.com>
2024-09-29 18:31:09 -0300

    Se agrega cupo = 30 y se instancia la lista de insciptos en el constructor

--- ClubDeportivo/ClubDeportivo/Actividades.cs
+++ ClubDeportivo/ClubDeportivo/Actividades.cs
@@ -8,10 +8,12 @@ namespace ClubDeportivo {
     internal class Actividades {
         public string nombreActividad {  get; set; }
         public sbyte id {  get; set; }
+        public sbyte cupo { get; set; }
         public List<Socios>Inscriptos { get; set; } 
 
         public Actividades(string nombre) { 
             nombreActividad = nombre.ToLower();
+            cupo = Convert.ToSByte(30);
             Inscriptos = new List<Socios>();
         }
     }
